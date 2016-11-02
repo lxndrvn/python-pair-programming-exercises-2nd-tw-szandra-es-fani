21f50f5bf72d2376d2005eeec7e349e38ac2425b
lxndrvn <i1990522@gmail.com>
2016-11-02 14:40:40 +0100

    status OK

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,10 +1,15 @@
-def palindrome(str):
-    return
+import re
 
+def palindrome(string):
+    word = string.lower()
+    word = re.sub(' ', '', word) 
+    if str(word) == str(word)[::-1]: 
+        return True
+    else:
+        return False
 
 def main():
     return
-
 
 if __name__ == '__main__':
     main()
