c17853ff5234c2b9eb5872004823ddcb669a913d
lxndrvn <i1990522@gmail.com>
2016-11-02 12:25:13 +0100

    solved

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,9 +1,18 @@
 def fizzbuzz(number):
-    return
-
+    x = number
+    if x % 15 == 0:
+        x = "FizzBuzz"
+    elif x % 3 == 0:
+        x = "Fizz"
+    elif x % 5 == 0:
+        x = "Buzz"
+    else:
+        print(x)
+    return x
 
 def main():
-    return
+    return True
 
 if __name__ == '__main__':
     main()
+
